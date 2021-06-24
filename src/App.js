d68b8e662de12a0592e4305f718adef56764ae8d
soobin <sooblair84@gmail.com>
2021-06-24 14:10:31 +0900

    app css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css"
 class App extends React.Component {
   state = {
     isLoading: true,
@@ -17,18 +18,27 @@ class App extends React.Component {
 
   render() {
     const { isLoading, movies } = this.state;
-    return <div>{isLoading ? "Loading..." : movies.map(movie => (
-      <Movie
-        key={movie.id}
-        id={movie.id}
-        title={movie.title}
-        year={movie.year}
-        summary={movie.summary}
-        poster={movie.medium_cover_image}
-      />
+    return <seactoion class="container">
+      {isLoading ? (<div class="loader">
+        <span class="loader__text">Loading...</span>
+      </div>
+      ) : (
+        <div class="movies">
+          {movies.map(movie => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              title={movie.title}
+              year={movie.year}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+            />
 
-    ))}
-    </div>
+          ))}
+        </div>
+      )}
+    </seactoion>
 
   }
 
--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie({ id, year, title, summary, poster }) {
-    return <h5>{title}</h5>
+function Movie({ year, title, summary, poster,genres }) {
+    return( <div className="movie">
+        <img src={poster} alt={title} title={title} />
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="genres">{genres.map((genre,index)=>(<li key={index} className="genres__genre">{genre}</li>))}</ul>
+        <p className="movie_summary">{summary}</p>
+        
+    </div>
+    );
 }
 
 Movie.propTypes = {
@@ -11,6 +19,7 @@ Movie.propTypes = {
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
+    genres:PropTypes.arrayOf(PropTypes.string).isRequired
 
 };
 
